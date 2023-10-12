cffb89cc80e6ee3985df566d8f064c7e43530a49
Jannis Roloff <jannis-rolli@msn.com>
2023-10-12 15:19:51 +0200

    fix: zylalabs colopallet api geändert zu colormind api

--- src/Functions/getApiColors.tsx
+++ src/Functions/getApiColors.tsx
@@ -5,23 +5,19 @@ import { setBackground, setPrimary } from "../redux/actions";
 export const useColors = () => {
   const dispatch = useDispatch();
 
-  const fetchColors = async (
-    apiKey = "2355|WToMUmoUgzTR5w3vGk7bbiqSptz7sUfrGrMwm4Q0"
-  ) => {
+  const fetchColors = async () => {
     try {
       const response = await axios({
-        method: "GET",
-        url:
-          "https://zylalabs.com/api/211/color+palette+api/221/get+random+palette",
-        headers: {
-          Authorization: `Bearer ${apiKey}`,
-        },
-        data: { model: "default" },
+        method: "POST",
+        url: "http://colormind.io/api/",
+        data: {
+          model: "default"
+        }
       });
       if (response.data.result && response.data.result.length >= 2) {
         // Konvertiere die ersten beiden Farbarrays in RGB-Strings
         const color1 = `rgb(${response.data.result[0].join(",")})`;
-        const color2 = `rgb(${response.data.result[3].join(",")})`;
+        const color2 = `rgb(${response.data.result[2].join(",")})`;
 
         // Verwende die konvertierten Farben für die Aktionen
         dispatch(setBackground(color1));
