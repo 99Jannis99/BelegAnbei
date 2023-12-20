1ee0c8bfb892ba402aad21002213253dbcbcd814
Dominic <dominic.roesmann@googlemail.com>
2023-12-20 16:07:55 +0100

    Kontaktseite, Textsnippets Component
    Kontaktseite => Versand selbst fehlt noch (nativ)
    Textsnippets Component => Embeddable <TextSnippet call="more-contact-top" />

--- src/Components/Screens/More/NotFoundScreen.js
+++ src/Components/Screens/More/NotFoundScreen.js
@@ -3,8 +3,11 @@ import { Text, View, SafeAreaView, StyleSheet } from "react-native";
 import { useSelector } from "react-redux";
 import Header from "../../shared/Header";
 
-function NotFoundScreen() {
+function NotFoundScreen({ route, navigation }) {
   const { background, primary } = useSelector((state) => state.colorReducer);
+
+  const givenParams = route.params
+
   return (
     <SafeAreaView style={[styles.moreSafeView, { backgroundColor: background }]}>
       <Header></Header>
@@ -13,6 +16,9 @@ function NotFoundScreen() {
       </View>
       <View style={styles.moreContent}>
         <Text>gtr t345t 3h94rh3498 rh34r3q409r hz4w0789fhrwpifuh4w309u8t z3qt03qzrh34q0ßrh3w4q0rh34wq098rh43798rz h34798r34 47 7ez 34qrz 349r z{"\n"}sdfsdfsdf</Text>
+      </View>
+      <View style={styles.moreContent}>
+        <Text>{JSON.stringify(givenParams, null, 2)}</Text>
       </View>
     </SafeAreaView>
   );
